143ea84b30194db7ac6703720ed43451ba1a8536
Lisot Nowak <lnowak@artemis-domaines.com>
2024-10-14 15:44:01 +0200

    initialiser symfony users

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/FirstController.php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+class FirstController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_accueil')]
+    public function number(): Response
+    {
+
+        return $this->render('accueil.html.twig');
+    }
+}
