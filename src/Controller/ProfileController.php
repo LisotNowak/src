c6c2d4d815255fd433b1b764438bc539c52ae5fd
Lisot Nowak <lnowak@artemis-domaines.com>
2025-10-30 13:49:03 +0100

    profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'user_profile')]
+    public function profile(): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('profile.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
